5fa16551a0453a8c7946fdd9ebca58e933874d23
AlyaSulimenko <87063914+AlyaSulimenko@users.noreply.github.com>
2022-06-01 15:31:32 +0200

    Added search js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,79 @@
 // Weather in current place(without icon)
+let actCity = document.querySelector("#actual-city");
+let actTemp = document.querySelector("#actual-temp");
+let actHumidity = document.querySelector("#actual-humidity");
+let actWind = document.querySelector("#actual-wind");
+let actCond = document.querySelector("#actual-conditions");
 let displayWeather = function (response) {
-  let actCity = document.querySelector("#actual-city");
-  let actTemp = document.querySelector("#actual-temp");
-  let actHumidity = document.querySelector("#actual-humidity");
-  let actWind = document.querySelector("#actual-wind");
-  let actCond = document.querySelector("#actual-conditions");
-
   actCity.innerHTML = response.data.name;
   actTemp.innerHTML = Math.round(response.data.main.temp);
   actHumidity.innerHTML = response.data.main.humidity;
-  actWind.innerHTML = response.data.wind.speed;
+  actWind.innerHTML = Math.round(response.data.wind.speed);
   actCond.innerHTML = response.data.weather[0].description;
   console.log(response);
 };
+
+//icon
+let displayWeatherIcon = function (response) {
+  let weatherIconBig = document.querySelector("#actual-icon");
+  weatherIconBig.classList.remove(weatherIconBig.classList.item(1));
+  if (response.data.weather[0].main === "Clouds") {
+    if (actHours < 21 || actHours > 4) {
+      weatherIconBig.classList.add("fa-cloud-sun");
+    } else {
+      weatherIconBig.classList.add("fa-cloud-moon");
+    }
+  } else if (response.data.weather[0].main === "Clear") {
+    if (actHours < 21 || actHours > 4) {
+      weatherIconBig.classList.add("fa-sun");
+    } else {
+      weatherIconBig.classList.add("fa-moon");
+    }
+  } else if (response.data.weather[0].main === "Drizzle") {
+    if (actHours < 21 || actHours > 4) {
+      weatherIconBig.classList.add("fa-sun-rain");
+    } else {
+      weatherIconBig.classList.add("fa-moon-rain");
+    }
+  } else if (response.data.weather[0].main === "Rain") {
+    weatherIconBig.classList.add("fa-cloud-showers-heavy");
+  } else if (response.data.weather[0].main === "Snow") {
+    weatherIconBig.classList.add("fa-snowflake");
+  } else if (response.data.weather[0].main === "Thunder") {
+    weatherIconBig.classList.add("fa-cloud-bolt");
+  } else if (response.data.weather[0].main === "Tornado") {
+    weatherIconBig.classList.add("fa-tornado");
+  } else {
+    weatherIconBig.classList.add("fa-smog");
+  }
+};
+
 let showCurCity = function (position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let key = "fb1611332d773db8e6829690cdae2059";
   let curUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
   axios.get(curUrl).then(displayWeather);
+  axios.get(curUrl).then(displayWeatherIcon);
+};
+let showActCity = function (event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  let key = "fb1611332d773db8e6829690cdae2059";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+  axios.get(apiUrl).then(displayWeatherIcon);
 };
 
 navigator.geolocation.getCurrentPosition(showCurCity);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", showActCity);
 
 //Current date & time
 let today = new Date();
 let dayDate = document.querySelector(".main__date");
 let time = document.querySelector(".main__time");
-
 let days = [
   "Sunday",
   "Monday",
@@ -64,3 +110,10 @@ if (actMinutes < 10) {
 }
 dayDate.innerHTML = `${actDay}, ${actMonth}, ${actDate}`;
 time.innerHTML = `${actHours}:${actMinutes}`;
+
+//Season Styling
+let seasonalBackground = document.querySelector(".container_seasonal");
+seasonalBackground.classList.remove(seasonalBackground.classList.item(2));
+if (actMonth === "June" || actMonth === "July" || actMonth === "August") {
+  seasonalBackground.classList.add("container_summer");
+}
