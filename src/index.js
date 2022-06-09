78166ec2ea8b6658fecd2ff69a6d4b18ebb0fc3c
AlyaSulimenko <87063914+AlyaSulimenko@users.noreply.github.com>
2022-06-09 12:48:00 +0200

    Added seasonal styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Multiplying the items in forecast with fake data
+//Multiplying the items in forecast
 let formatForecastDays = function (timestamp) {
   let date = new Date(timestamp * 1000);
   let days = [
@@ -40,6 +40,7 @@ let displayAllForecastItems = function (response) {
       } else {
         smallIcon = `<i class="fa-solid fa-smog icon_small"></i>`;
       }
+      let forecastTemp = Math.round(forecastDayOfWeek.temp.max);
       forecastHTML =
         forecastHTML +
         `<div class="col forecast__item">
@@ -48,9 +49,7 @@ let displayAllForecastItems = function (response) {
             )}</div>
             <div class="forecast__icon">${smallIcon}
             </div>
-            <div class="forecast__temperature">${Math.round(
-              forecastDayOfWeek.temp.max
-            )}°C</div>
+            <div class="forecast__temperature" id="forecast-temp">${forecastTemp} °C</div>
           </div>`;
     }
   });
@@ -58,7 +57,6 @@ let displayAllForecastItems = function (response) {
   forecastContent.innerHTML = forecastHTML;
 };
 
-//Displaying the real forecast
 let getForecast = function (coordinates) {
   let key = "fb1611332d773db8e6829690cdae2059";
   let forecastApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
@@ -193,31 +191,130 @@ let convertTemperature = function (event) {
 };
 convLink.addEventListener("click", convertTemperature);
 
-//Season Styling - haven't finished with it though
-let seasonalButton = document.querySelector(".search_button");
+//Seasons Styling (natural way and by clicking on link)
+let seasonalInput = document.querySelector("#search-input");
+let seasonalButton = document.querySelector("#search-btn");
 let seasonalBackground = document.querySelector(".landscape_background");
+let seasonalLink = document.querySelector("#link-source");
+let seasonalConvert = document.querySelector("#convert-temp");
 
 let seasonalMain = document.querySelector(".main__container");
 let seasonalSearch = document.querySelector(".search__container");
 let seasonalCredits = document.querySelector(".credits__container");
-
 let seasonalForecast = document.querySelector(".forecast__container");
+
+let linkWinter = document.querySelector("#seasons-winter");
+let linkSpring = document.querySelector("#seasons-spring");
+let linkSummer = document.querySelector("#seasons-summer");
+let linkAutumn = document.querySelector("#seasons-autumn");
+
 let showSummer = function () {
   seasonalBackground.classList.add("summer_background");
   seasonalButton.classList.add("summer_button");
+  seasonalInput.classList.add("summer_input");
+  seasonalLink.classList.add("summer_link");
+  seasonalConvert.classList.add("summer_convert");
+
   seasonalMain.classList.add("summer");
   seasonalSearch.classList.add("summer");
   seasonalCredits.classList.add("summer");
   seasonalForecast.classList.add("summer");
+
+  linkSpring.classList.add("seasons__item_summer");
+  linkSummer.classList.add("seasons__item_summer");
+  linkAutumn.classList.add("seasons__item_summer");
+  linkWinter.classList.add("seasons__item_summer");
+};
+let showWinter = function () {
+  seasonalBackground.classList.add("winter_background");
+  seasonalButton.classList.add("winter_button");
+  seasonalInput.classList.add("winter_input");
+  seasonalLink.classList.add("winter_link");
+  seasonalConvert.classList.add("winter_convert");
+
+  seasonalMain.classList.add("winter");
+  seasonalSearch.classList.add("winter");
+  seasonalCredits.classList.add("winter");
+  seasonalForecast.classList.add("winter");
+
+  linkSpring.classList.add("seasons__item_winter");
+  linkSummer.classList.add("seasons__item_winter");
+  linkAutumn.classList.add("seasons__item_winter");
+  linkWinter.classList.add("seasons__item_winter");
+};
+let showSpring = function () {
+  seasonalBackground.classList.add("spring_background");
+  seasonalButton.classList.add("spring_button");
+  seasonalInput.classList.add("spring_input");
+  seasonalLink.classList.add("spring_link");
+  seasonalConvert.classList.add("spring_convert");
+
+  seasonalMain.classList.add("spring");
+  seasonalSearch.classList.add("spring");
+  seasonalCredits.classList.add("spring");
+  seasonalForecast.classList.add("spring");
+
+  linkSpring.classList.add("seasons__item_spring");
+  linkSummer.classList.add("seasons__item_spring");
+  linkAutumn.classList.add("seasons__item_spring");
+  linkWinter.classList.add("seasons__item_spring");
+};
+let showAutumn = function () {
+  seasonalBackground.classList.add("autumn_background");
+  seasonalButton.classList.add("autumn_button");
+  seasonalInput.classList.add("autumn_input");
+  seasonalLink.classList.add("autumn_link");
+  seasonalConvert.classList.add("autumn_convert");
+
+  seasonalMain.classList.add("autumn");
+  seasonalSearch.classList.add("autumn");
+  seasonalCredits.classList.add("autumn");
+  seasonalForecast.classList.add("autumn");
+
+  linkSpring.classList.add("seasons__item_autumn");
+  linkSummer.classList.add("seasons__item_autumn");
+  linkAutumn.classList.add("seasons__item_autumn");
+  linkWinter.classList.add("seasons__item_autumn");
 };
 let resetSeason = function () {
   seasonalButton.classList.remove(seasonalButton.classList.item(1));
   seasonalBackground.classList.remove(seasonalBackground.classList.item(1));
+  seasonalInput.classList.remove(seasonalInput.classList.item(1));
+  seasonalLink.classList.remove(seasonalLink.classList.item(1));
+  seasonalConvert.classList.remove(seasonalConvert.classList.item(1));
+
   seasonalMain.classList.remove(seasonalMain.classList.item(2));
   seasonalSearch.classList.remove(seasonalSearch.classList.item(2));
   seasonalCredits.classList.remove(seasonalCredits.classList.item(2));
   seasonalForecast.classList.remove(seasonalForecast.classList.item(2));
+
+  linkSpring.classList.remove(linkSpring.classList.item(3));
+  linkSummer.classList.remove(linkSummer.classList.item(3));
+  linkAutumn.classList.remove(linkAutumn.classList.item(3));
+  linkWinter.classList.remove(linkWinter.classList.item(3));
 };
+
+let demoWinter = function (event) {
+  resetSeason();
+  showWinter();
+};
+let demoSpring = function (event) {
+  resetSeason();
+  showSpring();
+};
+let demoSummer = function (event) {
+  resetSeason();
+  showSummer();
+};
+let demoAutumn = function (event) {
+  resetSeason();
+  showAutumn();
+};
+
+linkWinter.addEventListener("click", demoWinter);
+linkSpring.addEventListener("click", demoSpring);
+linkSummer.addEventListener("click", demoSummer);
+linkAutumn.addEventListener("click", demoAutumn);
 
 let changeSeasons = function () {
   if (actMonth === "June" || actMonth === "July" || actMonth === "August") {
